066df8d3ebea8e073744abeaec880998cba95135
alex-costich <alejandro.costich@edu.uag.mx>
2024-07-05 17:48:12 -0700

    "Added simple filters"

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,20 +1,82 @@
+import { useState } from 'react'
 import UserRow from './UserRow.jsx'
 import style from './UsersList.module.css'
 
-const UsersList = ({ users, children }) => {
-	const usersRendered =
-		users.length > 0 ? (
-			users.map(user => <UserRow key={user.name} {...user} />)
-		) : (
-			<p>No hay usuarios</p>
-		)
+const UsersList = ({ users }) => {
+	const [search, setSearch] = useState('')
+	const [onlyActive, setOnlyActive] = useState(false)
+	const [sortBy, setSortBy] = useState(0)
+
+	let usersFiltered = filterUsersByName(users, search)
+	usersFiltered = filterActiveUsers(usersFiltered, onlyActive)
+	usersFiltered = sortUsers(usersFiltered, sortBy)
+	const usersRendered = renderUsers(usersFiltered)
 
 	return (
 		<div className={style.wrapper}>
-			{children}
+			<h1>Listado de usuarios</h1>
+			<form className={style.form}>
+				<input
+					type='text'
+					value={search}
+					onChange={ev => setSearch(ev.target.value)}
+				></input>
+				<div className='style.active'>
+					<input
+						type='checkbox'
+						checked={onlyActive}
+						onChange={ev => setOnlyActive(ev.target.checked)}
+					></input>
+					<span>Solo activos</span>
+				</div>
+				<select
+					value={sortBy}
+					onChange={ev => setSortBy(Number(ev.target.value))}
+				>
+					<option value={0}>Por defecto</option>
+					<option value={1}>Por nombre</option>
+				</select>
+			</form>
 			{usersRendered}
 		</div>
 	)
+}
+
+const filterUsersByName = (users, search) => {
+	if (!search) return users
+
+	const lowerCasedSearch = search.toLowerCase()
+
+	return users.filter(user =>
+		user.name.toLowerCase().startsWith(lowerCasedSearch)
+	)
+}
+
+const filterActiveUsers = (users, active) => {
+	if (!active) return [...users]
+
+	return users.filter(user => user.active)
+}
+
+const sortUsers = (users, sortBy) => {
+	const sortedUsers = [...users]
+
+	switch (sortBy) {
+		case 1:
+			return sortedUsers.sort((a, b) => {
+				if (a.name > b.name) return 1
+				if (a.name < b.name) return -1
+				return 0
+			})
+		default:
+			return sortedUsers
+	}
+}
+
+const renderUsers = users => {
+	if (users.length <= 0) return <p>No hay usuarios</p>
+
+	return users.map(user => <UserRow key={user.name} {...user} />)
 }
 
 export default UsersList
